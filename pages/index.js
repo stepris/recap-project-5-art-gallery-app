c088bf898a5024d2db17256d82c371bd4257bfc4
stepris <122841614+stepris@users.noreply.github.com>
2024-07-09 15:02:58 +0200

    Change index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import useSWR from "swr";
 import ArtPieces from "@/components/ArtPieces";
 import Spotlight from "@/components/Spotlight";
-import getRandomArrayElement from "@/utils/getRandomArrayElement";
 
 export default function HomePage() {
   const URL = "https://example-apis.vercel.app/api/art";
