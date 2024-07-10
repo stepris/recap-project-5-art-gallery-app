489f3280aa55a2cbcc8a0cc6017d9b1bcc4a647e
stepris <122841614+stepris@users.noreply.github.com>
2024-07-10 08:43:19 +0200

    Add DetailsPage and use router

---
+++ components/ArtPieceDetails/index.js
@@ -0,0 +1,18 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function ArtPieceDetails({ image, title, artist, year, genre }) {
+  return (
+    <>
+      <Image src={image} alt={title} height={300} width={300} />
+      <p>
+        {artist}: {title}
+      </p>
+      <p>Year: {year}</p>
+      <p>Genre: {genre}</p>
+      <Link href="/art-pieces">
+        <button type="button">Back</button>
+      </Link>
+    </>
+  );
+}
